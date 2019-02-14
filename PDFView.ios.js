1794ba5af0fc6f45c4b4f4c7e49887bf9411a65e
Danila Nikonec <Desintegrator@users.noreply.github.com>
2019-02-14 17:08:28 +0500

    init

---
+++ PDFView.ios.js
@@ -0,0 +1,30 @@
+'use strict';
+import React,{ Component } from 'react';
+import { requireNativeComponent, View } from 'react-native';
+
+class PDFView extends Component {
+  constructor(props) {
+    super(props);
+    this._onChange = this._onChange.bind(this);
+  }
+
+  setNativeProps(nativeProps) {
+    this._root.setNativeProps(nativeProps);
+  }
+
+  _onChange(event:Event) {
+    this.props.onLoadComplete && this.props.onLoadComplete(Number(event.nativeEvent.message));
+  }
+
+  render() {
+    return <PDFCustomView ref={component => this._root = component} {...this.props} onChange={this._onChange}/>;
+  }
+}
+
+//var PDFCustomView = requireNativeComponent('RNPDFView', null);
+
+var PDFCustomView = requireNativeComponent('RNPDFView', PDFView, {
+  nativeOnly: {onChange: true}
+});
+
+export default PDFView;
